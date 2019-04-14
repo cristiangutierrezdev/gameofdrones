40f6b150f84848a2c209d9a2d72b147b5b0b4441
3pyp <cristiandisenadorvisual@gmail.com>
2019-04-14 15:02:33 -0500

    server finished and react app created

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,8 +3,11 @@ const router = require("./routes");
 const cors = require("cors");
 const morgan = require("morgan");
 const config = require("./config");
+const { errors } = require('celebrate')
 const { dbConnection } = require("./models");
 const app = express();
+
+const PORT = process.env.PORT || config.storageConfig.port
 
 app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +15,14 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1", router);
-// app.use(errors())
+app.use(errors())
 
 dbConnection
   .then(result => {
-    app.listen(config.storageConfig.port, () => {
-      console.log(`Server started on port ${config.storageConfig.port}`);
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
     });
   })
-  .catch(err => {
+  .catch(err => { 
     console.log(err);
   });
---
+++ Server/src/midlewares/validations.js
@@ -0,0 +1,15 @@
+const { celebrate, Joi } = require('celebrate')
+
+module.exports = {
+    createPlayerValidator: celebrate({
+        body: Joi.object().keys({
+          name: Joi.string().required()
+        })
+      }),
+    createGameValidator: celebrate({
+        body: Joi.object().keys({
+            player_one: Joi.string().required(),
+            player_two: Joi.string().required()
+        })
+      })
+}
