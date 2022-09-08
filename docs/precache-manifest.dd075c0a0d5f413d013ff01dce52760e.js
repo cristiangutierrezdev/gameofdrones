c8740e34f166761dc062971cef1308ec81a026b2
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2022-09-08 01:53:27 -0500

    Added github pages docs folder.

---
+++ docs/precache-manifest.dd075c0a0d5f413d013ff01dce52760e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e51b00290e30907083b2",
+    "url": "/static/css/main.568a0e0c.chunk.css"
+  },
+  {
+    "revision": "e51b00290e30907083b2",
+    "url": "/static/js/main.b7990a41.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "9a27697a57a854d97210",
+    "url": "/static/js/2.438019f7.chunk.js"
+  },
+  {
+    "revision": "2f1f1025e303298eeafa6da775eb289a",
+    "url": "/index.html"
+  }
+];
